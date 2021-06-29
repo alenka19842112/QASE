bdf6636443e731e208bd5bf58b30b0e9d12e5961
aivanov <aivanov@aras.com>
2021-06-29 08:09:06 +0300

    create New Test Suite And Delete Test

--- src/main/java/adapters/SuiteAdapter.java
+++ src/main/java/adapters/SuiteAdapter.java
@@ -4,7 +4,6 @@ import objects.TestSuite;
 
 public class SuiteAdapter extends BaseAdapter {
     private static final String SUITE_URI = "suite/";
-    private static final String DELETE_URI ="";
 
     public int create(String projectCode, TestSuite testSuite) {
         return post(SUITE_URI + projectCode, converter.toJson(testSuite)).body().path("result.id");
